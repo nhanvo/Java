65e3c48ef2af719836599a1246dfb81f84d0044e
nhanvo <vothanhnhan.it@gmail.com>
2016-07-13 20:41:34 +0700

    Add most of example with string

--- JavaCore/StringExample/StringExample.java
+++ JavaCore/StringExample/StringExample.java
@@ -1,5 +1,45 @@
 public class StringExample {
-  public static void main(String[] args) {
-        // TODO::
+    public static void main(String[] args) {
+        StringExample strEx = new StringExample();
+
+        // Compare String
+        strEx.StringCompareEmp();
+
+        // Search last string
+        strEx.SearchLastString();
+
+        // Remove char at any position
+        strEx.RemoveCharAt();
+    }
+    
+    void StringCompareEmp() {
+        String str = "Hello World";
+        String anotherString = "Hello world";
+        
+        Object objStr = str;
+        System.out.println(str.compareTo(anotherString));
+        System.out.println(str.compareToIgnoreCase(anotherString));
+        System.out.println(str.compareTo(objStr.toString()));
+    }
+
+    void SearchLastString() {
+        String strOrig = "Hello world, Hello Reader";
+        int lastIndex = strOrig.lastIndexOf("Hello");
+        String result = "";
+        if (lastIndex == -1) {
+            result = "Hello not found";
+        } else {
+            result = "Last occurrence of Hello is at index " + lastIndex;
+        }
+
+        System.out.println(result);
+    }
+
+    void RemoveCharAt() {
+        String str = "This is Java";
+        System.out.println("Original string: " + str);
+        int pos = 3;
+        str = str.substring(0, pos) + str.substring(pos + 1);
+        System.out.println("Remove character number 3: " + str);
     }
 }
