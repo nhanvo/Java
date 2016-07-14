96f4e278125dfc50285e4b7e116f68461eff4b7f
nhanvo <vothanhnhan.it@gmail.com>
2016-07-15 02:10:04 +0700

    Add reference page

--- DesignPattern/src/com/nhanvo/factory/PC.java
+++ DesignPattern/src/com/nhanvo/factory/PC.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Reference from: http://www.journaldev.com/1392/factory-design-pattern-in-java
  */
 package com.nhanvo.factory;
 
