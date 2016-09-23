a283d74bd2b46be39b6230d26499b94fbe0db80c
nhanvo <vothanhnhan.it@gmail.com>
2016-09-23 12:49:00 +0700

    Add Spring Hibernate example with anotation

---
+++ SpringMVC/SpringHibernateOneToManyAnotations/src/main/java/com/nhanvo/example/stock/StockDailyRecord.java
@@ -0,0 +1,109 @@
+package com.nhanvo.example.stock;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name="stock_daily_record", catalog="mkyongdb", uniqueConstraints=@UniqueConstraint(columnNames="DATE"))
+public class StockDailyRecord implements Serializable{
+	private Integer recordId;
+	private Stock stock;
+	private Float priceOpen;
+	private Float priceClose;
+	private Float priceChange;
+	private Long volume;
+	private Date date;
+	
+	public StockDailyRecord() {
+		
+	}
+	
+	public StockDailyRecord(Stock stock, Float priceOpen, Float priceClose,
+			Float priceChange, Long volume, Date date) {
+		this.stock = stock;
+		this.priceOpen = priceOpen;
+		this.priceClose = priceClose;
+		this.priceChange = priceChange;
+		this.volume = volume;
+		this.date = date;
+	}
+
+	@Id
+	@GeneratedValue(strategy=IDENTITY)
+	@Column(name="RECORD_ID", unique=true, nullable=false)
+	public Integer getRecordId() {
+		return recordId;
+	}
+
+	public void setRecordId(Integer recordId) {
+		this.recordId = recordId;
+	}
+
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="STOCK_ID", nullable=false)
+	public Stock getStock() {
+		return stock;
+	}
+
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+
+	@Column(name="PRICE_OPEN", precision=6)
+	public Float getPriceOpen() {
+		return priceOpen;
+	}
+
+	public void setPriceOpen(Float priceOpen) {
+		this.priceOpen = priceOpen;
+	}
+
+	@Column(name = "PRICE_CLOSE", precision = 6)
+	public Float getPriceClose() {
+		return priceClose;
+	}
+
+	public void setPriceClose(Float priceClose) {
+		this.priceClose = priceClose;
+	}
+
+	@Column(name = "PRICE_CHANGE", precision = 6)
+	public Float getPriceChange() {
+		return priceChange;
+	}
+
+	public void setPriceChange(Float priceChange) {
+		this.priceChange = priceChange;
+	}
+
+	@Column(name = "VOLUME")
+	public Long getVolume() {
+		return volume;
+	}
+
+	public void setVolume(Long volume) {
+		this.volume = volume;
+	}
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "DATE", unique = true, nullable = false, length = 10)
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}	
+}
