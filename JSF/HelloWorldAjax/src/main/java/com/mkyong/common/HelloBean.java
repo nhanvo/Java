e13e37aa3155f72b16a55113c8202b2ed8c5a493
nhanvo <vothanhnhan.it@gmail.com>
2017-05-14 01:13:56 +0700

    Change most of file

---
+++ JSF/HelloWorldAjax/src/main/java/com/mkyong/common/HelloBean.java
@@ -0,0 +1,17 @@
+package com.mkyong.common;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	
+	
+}
