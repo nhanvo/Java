cecc04710076fc02d603f28d3d8fbf5433f07019
nhanvo <vothanhnhan.it@gmail.com>
2017-05-14 00:39:59 +0700

    Implement Hello bean

--- JSF/HelloWorld/src/main/java/com/mkyong/common/HelloBean.java
+++ JSF/HelloWorld/src/main/java/com/mkyong/common/HelloBean.java
@@ -1,5 +1,24 @@
 package com.mkyong.common;
 
-public class HelloBean {
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
